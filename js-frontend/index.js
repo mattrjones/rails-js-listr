bac75d14e42de6beccd19795b981fc1d6a3dc19f
Matthew Jones <matts3371@gmail.com>
2021-02-05 20:03:29 -0600

    added create list function

--- js-frontend/index.js
+++ js-frontend/index.js
@@ -10,10 +10,46 @@ const BASE_URL = "http://localhost:3000"
 function fetchLists(){
     fetch(`${BASE_URL}/lists`)
     .then(resp => resp.json())
-    .then(users => {
+    .then(lists => {
         for(const list of lists){
-            let i = new List(list.name)
+            let i = new List(list.id, list.name)
             i.renderList();
         }
+    })
+}
+
+function createForm(){
+    let listsForm = document.getElementById("lists-form")
+
+    listsForm.innerHTML += 
+    `
+    <form>
+        List Name: <input type="text" id="name"><br>
+        <input type="submit" value="Create List">
+    </form>
+    `
+
+    listsForm.addEventListener("submit", listFormSubmission)
+}
+
+function listFormSubmission(){
+    debugger;
+    event.preventDefault();
+    let name = document.getElementById("name").value
+
+    let list = {name: name}
+
+    fetch(`${BASE_URL}/lists`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(list)
+    })
+    .then(resp => resp.json())
+    .then(list => {
+        let i = new List(list.id, list.name)
+        i.renderList();
     })
 }
--- js-frontend/src/list.js
+++ js-frontend/src/list.js
@@ -1,5 +1,6 @@
 class List {
-    constructor(name){
+    constructor(id, name){
+        this.id = id;
         this.name = name;
     }
 
