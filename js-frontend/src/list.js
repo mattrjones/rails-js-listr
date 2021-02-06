27fd1e9070578b267eb8dc27be9b38c376b5da0a
Matthew Jones <matts3371@gmail.com>
2021-02-05 20:52:53 -0600

    began working on displaying tasks in lists

--- js-frontend/src/list.js
+++ js-frontend/src/list.js
@@ -13,8 +13,18 @@ class List {
         `
         <ul>
         <h2>${this.name}:</h2>
+        <div id="task-form">
+        </div>
         </ul>
+        <button class="task-button" onClick="taskForm()">Add A Task</button>
         <button class="delete-button" data-id=${this.id} onClick="deleteList()">Delete List</button>
         `
+
+        // this.tasks.each do |task|
+        //     listsDiv.innerHTML += 
+        //     `
+        //     <li>${task.label}</li>
+        //     `
+        // end 
     }
 }
