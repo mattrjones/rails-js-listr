4f0dc68e6c5e656f43dd8034cb179cd8c7ac4946
Matthew Jones <matts3371@gmail.com>
2021-02-10 17:10:32 -0600

    added editList

--- js-frontend/src/list.js
+++ js-frontend/src/list.js
@@ -6,33 +6,121 @@ const listFormFields = `
 `;
 
 class List {
-    constructor(id, name){
-        this.id = id;
-        this.name = name;
+    constructor(data){
+        this.id = data.id;
+        this.name = data.name;
+        this.tasks = data.tasks.sort((a,b) => (a.updated_at < b.updated_at) ? 1 : ((b.updated_at) ? -1 : 0));
     }
 
-    //render list instance method
-
-    renderList(){
-        let listsDiv = document.getElementById("lists-container")
-
-        listsDiv.innerHTML += 
-        `
-        <ul>
-        <h2>${this.name}:</h2>
-        <div id="task-form">
-        </div>
-        </ul>
-        <button class="task-button" onClick="taskForm()">Add A Task</button>
-        <button class="delete-button" data-id=${this.id} onClick="deleteList()">Delete List</button>
-        `
-
-        // this.tasks.each do |task|
-        //     listsDiv.innerHTML += 
-        //     `
-        //     <li>${task.label}</li>
-        //     `
-        // end 
+    //static class-only methods
+    static newListForm() {
+        let newListFormDiv = document.getElementById('list-form')
+        newListFormDiv.innerHTML = `
+        <form onsubmit="createList(); return false;">` + 
+            listFormFields + 
+            `<input class="button" type="submit" value="Add New Category">
+        </form>
+        <br/>`
     }
+
+    static editListForm(){
+        let editListFormDiv = document.getElementById('list-form')
+        editListFormDiv.innerHTML = `
+        <form onsubmit="updateList(); return false;">` + 
+            listFormFields + 
+            `<input type="submit" value="Update Info">
+        </form>
+        <br/>`
+    }
+
+    // //render list instance method
+
+    // renderList(){
+    //     let listsDiv = document.getElementById("lists-container")
+
+    //     listsDiv.innerHTML += 
+    //     `
+    //     <ul>
+    //     <h2>${this.name}:</h2>
+    //     <div id="task-form">
+    //     </div>
+    //     </ul>
+    //     <button class="task-button" onClick="taskForm()">Add A Task</button>
+    //     <button class="delete-button" data-id=${this.id} onClick="deleteList()">Delete List</button>
+    //     `
+
+    //     // this.tasks.each do |task|
+    //     //     listsDiv.innerHTML += 
+    //     //     `
+    //     //     <li>${task.label}</li>
+    //     //     `
+    //     // end 
+    // }
 
 }
+
+function getLists(){
+    fetch("http://localhost:3000/lists")
+        .then(resp => resp.json())
+        .then(data => {
+            renderListsHtml(data)
+            addListsClickListeners()
+            addTasksClickListeners()
+        })
+}
+
+function createList(){
+    const list = {
+        title: document.getElementById('name').value, 
+        description: document.getElementById('description').value,
+    }
+
+    fetch("http://localhost:3000/lists", {
+        method: 'POST',
+        body: JSON.stringify(list),
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    })
+        .then(resp => resp.json() )
+        .then(list => {
+            clearListsHtml()
+            getLists()
+            List.newListForm()
+        });
+}
+
+function updateList() {
+    let ListId = this.event.target.listID.value 
+
+    const list = {
+        title: document.getElementById('name').nodeValue,
+        description: document.getElementById('description').nodeValue,
+    }
+
+    fetch(`http://localhost:3000/lists/${listID}`, {
+        method: 'PATCH',
+        body: JSON.stringify(list),
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    })
+        .then(resp => resp.json())
+        .then(list => {
+            clearListsHtml()
+            getLists()
+            List.newListForm()
+        });
+}
+
+function editList(){
+    let listId = this.parentElement.getAttribute('data-list-id')
+
+    fetch(`http://localhost:3000/lists/${listId}`)
+        .then(resp => resp.json())
+        .then(data => {
+            List.editListForm()
+            let listForm = document.getElementById('list-form')
+            listForm.querySelector('#name').value = data.name 
+            listForm.querySelector('#listId').value = data.id 
+            listForm.querySelector('#description').value = data.description 
+        })
+}
+
+
